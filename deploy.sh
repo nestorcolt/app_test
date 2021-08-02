27673730f32d0b6f40fc8e029d2a42162b01c16a
nestorcolt <nestor.colt@gmail.com>
2021-08-02 17:35:32 +0200

    working version with local machine ssl certificates

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get the user machine in a env variable
+MACHINE_USER=${USER}
+
+# Install mkcert to validate ssl configuration 
+sudo apt update
+sudo apt install -y curl libnss3-tools
+sudo apt clean
+sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+sudo curl -Lo /usr/local/bin/mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.4.0/mkcert-v1.4.0-linux-amd64
+sudo chmod +x /usr/local/bin/mkcert
+
+mkdir -p /home/${MACHINE_USER}/nginx/ssl/
+mkcert -install
+mkcert -key-file /home/${MACHINE_USER}/nginx/ssl/ssl.key -cert-file /home/${MACHINE_USER}/nginx/ssl/ssl.crt localhost 127.0.0.1
+
+chmod 644 /usr/local/share/ca-certificates/*.crt
+update-ca-certificates
+
+# Create the .env file to make sure it exist for next validation
+touch .env
+
+# Get the local user name to pass later on path as env variable
+USERNAME_EXISTS="$(cat .env | grep -c USERNAME)"
+
+if [[ $USERNAME_EXISTS = 0 ]]
+then 
+    echo USERNAME=${MACHINE_USER} >> .env
+fi
+
+# Build dockerized apps and containers
+sudo docker-compose up --build --force-recreate
